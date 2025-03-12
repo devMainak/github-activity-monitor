d1ff58b22e035a00a72f0165f76cc42a844e994f
devMainak <mrterm404@gmail.com>
2025-03-12 11:58:59 +0530

    feat: improved error handling

--- src/controller/git.controller.js
+++ src/controller/git.controller.js
@@ -13,7 +13,11 @@ const githubApi = axios.create({
 exports.getUserDetails = async (req, res) => {
   try {
     const response = await githubApi.get(`/users/${username}`);
-    res.status(200).json(response.data);
+    if (response.data) {
+      res.status(200).json(response.data);
+    } else {
+      res.status(400).json({ error: "Failed to fetch user's github data" });
+    }
   } catch (error) {
     res.status(500).json({ error: "Error fetching Github data" });
   }
@@ -23,9 +27,13 @@ exports.getRepoDetails = async (req, res) => {
   try {
     const { repo } = req.params;
     const response = await githubApi.get(`/repos/${username}/${repo}`);
-    res.status(200).json(response.data);
+    if (response.data) {
+      res.status(200).json(response.data);
+    } else {
+      res.status(400).json({ error: "Failed to repository data." });
+    }
   } catch (error) {
-    res.json(500).json({ error: "Error fetching repository data." });
+    res.status(500).json({ error: "Error fetching repository data." });
   }
 };
 
@@ -33,11 +41,17 @@ exports.createIssue = async (req, res) => {
   try {
     const { repo } = req.params;
     const { title, body } = req.body;
+
+    if (!title || !body) {
+      return res.status(400).json({ error: "Title and body are required" });
+    }
+
     const response = await githubApi.post(`/repos/${username}/${repo}/issues`, {
       title,
       body,
     });
-    res.status(201).json({ issue_url: response.data.html_url });
+
+    return res.status(201).json({ issue_url: response.data.html_url });
   } catch (error) {
     res.status(500).json({ error: "Error creating Github issue" });
   }
